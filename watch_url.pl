24ab334e0d176b1658e6559d59a4175b1b0407f2
Hari Sekhon <harisekhon@gmail.com>
2013-01-04 17:21:53 +0000

    updated watch_url.pl

--- watch_url.pl
+++ watch_url.pl
@@ -10,23 +10,20 @@
 
 # Utility to watch a given URL and output it's status code. Useful for testing web farms and load balancers
 
-# TODO: reintegrate with HariSekhonUtils
-
-$main::VERSION = "0.1";
+$VERSION = "0.2";
 
 use strict;
 use warnings;
-use File::Basename;
-use Getopt::Long qw(:config bundling);
+BEGIN {
+    use File::Basename;
+    use lib dirname(__FILE__) . "/lib";
+}
+use HariSekhonUtils;
 use LWP::UserAgent;
 use POSIX;
 use Time::HiRes qw/sleep time/;
 
-my $help;
-my $host;
-my $msg;
 my $count = 0;
-my $port;
 my $res;
 my $returned = 0;
 my $sleep_secs = 1;
@@ -35,65 +32,52 @@ my $status_line;
 my $time;
 my $total = 0;
 my $url;
-my $verbose = 0;
-my $version;
 my %stats;
 my $time_taken;
 my $tstamp1;
 my $tstamp2;
 
-my $progname = basename $0; 
+$usage_line = "usage: $progname --url 'http://host/blah' --sleep-interval=1 --count=0 (unlimited)";
 
-sub usage{
-    die "usage: $progname -u 'http://host/blah' --sleep-interval=1 --count=0 (unlimited)\n";
-}
+%options = (
+    "u|url=s"               => [ \$url,         "URL to GET in http(s)://host/page.html form" ],
+    "s|sleep-interval=f"    => [ \$sleep_secs,  "Sleep interval in seconds between URL requests (default: 1)" ],
+    "c|count=i"             => [ \$count,       "Number of times to request the given URL (default: 0 for unlimited)" ],
+);
+@usage_order=qw/url sleep-interval count/;
 
-sub vlog{
-    print "@_\n" if $verbose;
-}
+delete $HariSekhonUtils::options2{"t|timeout=i"};
 
-GetOptions (
-            "h|help|usage"          => \$help,
-            "c|count=i"             => \$count,
-            "s|sleep-interval=s"    => \$sleep_secs,
-            "u|url=s"               => \$url,
-            "v|verbose+"            => \$verbose,
-            "V|version"             => \$version,
-           ) or usage;
+get_options();
 
-usage if defined($help);
-die "Version $main::VERSION\n" if defined($version);
+#defined($url) or usage;
+#$url =~ /^(http:\/\/\w[\w\.-]+\w(?:\/[\w\.\;\=\&\%\/-]*)?)$/ or die "Invalid URL given\n";
+#$url = $1;
 
-$count =~ /^(\d+)$/ or die "Invalid count given\n";
-$count = $1;
+$url = validate_url($url);
+isInt($count)        or die "Invalid count given, must be a positive integer";
+isFloat($sleep_secs) or die "Invalid sleep interval given, must be a positive floating point number";
 
-$sleep_secs =~ /^(\d+(?:\.\d+)?)$/ or die "Invalid sleep interval given\n";
-$sleep_secs = $1;
-
-defined($url) or usage;
-$url =~ /^(http:\/\/\w[\w\.-]+\w(?:\/[\w\.\;\=\&\%\/-]*)?)$/ or die "Invalid URL given\n";
-$url = $1;
-print "Watch URL: $url\n";
-print "Count: ";
-print "$count";
-print " (unlimited)" if ($count eq 0);
-print "\n";
-print "Sleeping for $sleep_secs seconds between attempts\n\n";
+vlog_options "Count", $count ? $count : "$count (unlimited)";
+vlog_options "Sleep Interval", $sleep_secs;
+vlog2 "\nSleeping for $sleep_secs seconds between attempts\n\n";
 
 my $ua = LWP::UserAgent->new;
 $ua->agent("Hari Sekhon Watch URL version $main::VERSION ");
 my $req = HTTP::Request->new(GET => $url);
 
+print "="x133 . "\n";
 #print "Time\t\t\tCount\t\tResult\t\tHTTP Status Code = Number (% of Total Requests, % of Returned Requests)\n";
 print "Timestamp\t\tCount\t\tResult\t\tRound Trip Time\t\tHTTP Status Code = % of Total Requests (number/total)\n";
+print "="x133 . "\n";
 #while(1){
 for(my $i=1;$i<=$count or $count eq 0;$i++){
     $time = strftime("%F %T", localtime);
-    vlog "sending request";
+    vlog2 "* sending request";
     $tstamp1 = time;
     $res     = $ua->request($req);
     $tstamp2 = time;
-    vlog "got response";
+    vlog2 "* got response";
     $status  = $status_line  = $res->status_line;
     $status  =~ s/\s.*$//;
     $total++;
@@ -114,6 +98,6 @@ for(my $i=1;$i<=$count or $count eq 0;$i++){
         $msg .= "$_ = " . int($stats{$_} / $total * 100) . "% ($stats{$_}/$total)\t\t";
     }
     print "$time\t$i\t\t$msg\n";
-    vlog "sleeping for $sleep_secs seconds";
+    vlog2 "* sleeping for $sleep_secs seconds";
     sleep $sleep_secs;
 }
