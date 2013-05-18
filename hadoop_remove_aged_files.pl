f3c7be00fbf3a8542dd03f2c814b014a02aaf31c
Hari Sekhon <harisekhon@gmail.com>
2013-05-18 21:14:20 +0100

    updated hadoop_remove_aged_files.pl

--- hadoop_remove_aged_files.pl
+++ hadoop_remove_aged_files.pl
@@ -12,7 +12,7 @@ $DESCRIPTION = "Deletes files from Hadoop's HDFS /tmp directory that are older t
 
 Credit to my old colleague Rob Dawson @ Specific Media for giving me this idea during lunch";
 
-$VERSION = "0.1";
+$VERSION = "0.2";
 
 use strict;
 use warnings;
@@ -31,61 +31,74 @@ my $hadoop_bin = $default_hadoop_bin;
 my $DEFAULT_PATH = "/tmp";
 my $path = $DEFAULT_PATH;
 
-my $days;
+my $days  = 0;
+my $hours = 0;
+my $mins  = 0;
 my $exclude;
-my $expunge = 0;
+my $skipTrash = "";
+my $rm = 0;
 
 set_timeout_max(86400);    # 1 day max -t timeout
 set_timeout_default(1800); # 30 mins. hadoop fs -lsr /tmp took 6 minutes to list 1720943 files/dirs on my test cluster!
 
 my %months = (
-    "Jan" => 0,
-    "Feb" => 1,
-    "Mar" => 2,
-    "Apr" => 3,
-    "May" => 4,
-    "Jun" => 5,
-    "Jul" => 6,
-    "Aug" => 7,
-    "Sep" => 8,
-    "Oct" => 9,
-    "Nov" => 10,
-    "Dec" => 11
+    "Jan" => 1,
+    "Feb" => 2,
+    "Mar" => 3,
+    "Apr" => 4,
+    "May" => 5,
+    "Jun" => 6,
+    "Jul" => 7,
+    "Aug" => 8,
+    "Sep" => 9,
+    "Oct" => 10,
+    "Nov" => 11,
+    "Dec" => 12
 );
 
 %options = (
-    "d|days=f"      =>  [ \$days,       "Number of days after which to delete files, can be a float" ],
+    "d|days=i"      =>  [ \$days,       "Number of days after which to delete files" ],
+    "H|hours=i"     =>  [ \$hours,      "Number of hours after which to delete files" ],
+    "m|mins=i"      =>  [ \$mins,       "Number of minutes after which to delete files" ],
     "p|path=s"      =>  [ \$path,       "Path for which to remove old files (default: $DEFAULT_PATH)" ],
     "e|exclude=s"   =>  [ \$exclude,    "Regex of files to exclude from being deleted" ],
-    "expunge"       =>  [ \$expunge,    "Call expunge after deletion. By default things are deleted to trash so they don't actually reclaim the space without this switch" ],
+    "rm"            =>  [ \$rm,         "Actually launch the hadoop fs -rm commands on the files, by default this script only prints the hadoop fs -rm commands. WARNING: only use this switch after you have checked what the list of files to be removed is, otherwise you may lose data" ],
+    "skipTrash"     =>  [ \$skipTrash,  "Skips moving files to HDFS Trash, reclaims space immediately" ],
     "hadoop-bin=s"  =>  [ \$hadoop_bin, "Path to 'hadoop' command if not in \$PATH" ],
 );
-@usage_order = qw/days path exclude expunge hadoop-bin/;
+@usage_order = qw/days hours mins path exclude rm skipTrash hadoop-bin/;
 get_options();
 
-my $echo = "";
-$echo = "echo" if $debug;
+my $echo = "echo";
+if ($rm and not $debug){
+    $echo = ""; # actually run the hadoop fs -rm command instead of just echo'ing it out
+}
+$skipTrash = "-skipTrash" if $skipTrash;
 
-$days    = validate_float($days, 0.003, 3650, "days");
-$path    = validate_filename($path); # because validate_dir[ectory] checks the directory existance on the local filesystem
+$days    = validate_float($days,  0, 3650, "days");
+$hours   = validate_float($hours, 0, 23,   "hours");
+$mins    = validate_float($mins,  0, 59,   "mins");
+my $max_age_secs = ($days * 86400) + ($hours * 3600) + ($mins * 60);
+usage "must specify a total max age > 5 minutes" if ($max_age_secs < 300);
+$path    = validate_filename($path, undef, "path"); # because validate_dir[ectory] checks the directory existance on the local filesystem
 $exclude = validate_regex($exclude) if defined($exclude);
 $hadoop_bin  = which($hadoop_bin, 1);
 $hadoop_bin  =~ /\b\/?hadoop$/ or die "invalid hadoop program '$hadoop_bin' given, should be called hadoop!\n";
-vlog_options "path", $path;
-vlog_options "expunge", $expunge ? "true" : "false";
+vlog_options "rm", $rm ? "true" : "false";
+vlog_options "skipTrash", $skipTrash ? "true" : "false";
 vlog_options "hadoop path", $hadoop_bin;
 vlog2;
 
 set_timeout();
 
-my $cmd = "hadoop fs -lsr '$path'";
+my $cmd = "hadoop fs -ls -R '$path'";
 my $fh = cmd("$cmd | ") or die "ERROR: $? returned from \"$cmd\" command: $!\n";
 my @files = ();
 my $now   = time || die "Failed to get epoch timestamp\n";
 my $file_count    = 0;
 my $files_removed = 0;
 while (<$fh>){
-    print "output: $_" if $verbose > 3;
+    print "output: $_" if $verbose >= 3;
     chomp;
     my $line = $_;
     $line =~ /^Found\s\d+\sitems/ and next;
@@ -99,25 +112,25 @@ while (<$fh>){
         my $hour     = $5;
         my $min      = $6;
         my $filename = $7;
-        my $tstamp   = timegm(0, $min, $hour, $day, $months{$month}, $year-1900) || die "$progname: Failed to convert timestamp $year-$month-$day $hour:$min for comparison\n";
-        if( ($now - $tstamp ) > ($days * 86400) ){
+        $month = $months{$month} if grep { $month eq $_} keys %months;
+        my $tstamp   = timelocal(0, $min, $hour, $day, $month-1, $year) || die "$progname: Failed to convert timestamp $year-$month-$day $hour:$min for comparison\n";
+        if( ($now - $tstamp ) > $max_age_secs){
             next if (defined($exclude) and $filename =~ /$exclude/);
+            # Some additional safety stuff
+            next if ($filename =~ /^\/(?:tmp\/mapred|hbase)\//i);
             push(@files, $filename); 
             $files_removed++;
         }
     } else {
         warn "$progname: WARNING - failed to match line from hadoop output: \"$line\"\n";
     }
-    if(scalar @files >= 20){
-        $cmd = "$echo echo hadoop fs -rm '" . join("' '", @files) . "'";
+    if(@files){
+        $cmd = "$echo hadoop fs -rm $skipTrash '" . join("' '", @files) . "'";
         system($cmd) and die "ERROR: $? returned from \"hadoop fs -rm\" command: $!\n";
         @files = ();
     }
 }
 
-if($expunge){
-    $cmd = "$echo echo hadoop fs -expunge";
-    system($cmd) and warn "$progname: Expunge failed, returned $? from \"$cmd\" command: $!\n";
-}
-$msg = "$progname Complete - %d files checked, %d files older than %s days " . ($echo ? "" : "removed") . "\n";
-printf($msg, $file_count, $files_removed, $days);
+plural($files_removed);
+$msg = "$progname Complete - %d files checked, %d file$plural older than %s days %s hours %s mins " . ($echo ? "" : "removed") . "\n";
+printf($msg, $file_count, $files_removed, $days, $hours, $mins);
