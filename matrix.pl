edcc5a6d6280730770820827937453eb697d018e
Hari Sekhon <harisekhon@gmail.com>
2013-12-20 22:55:51 +0000

    added matrix.pl

---
+++ matrix.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl -T
+#
+#  Author: Hari Sekhon
+#  Date: 2013-12-19 18:21:24 +0000 (Thu, 19 Dec 2013)
+#
+#  http://github.com/harisekhon
+#
+#  License: see accompanying LICENSE file
+#
+
+$DESCRIPTION = "Prints a cool Matrix effect in your terminal
+
+Thanks to my colleagues Chris Greatbanks and Sameer Charania at BSkyB for sharing this cool web tip with me on which I decided to base this code:
+
+http://www.climagic.org/coolstuff/matrix-effect.html";
+
+$VERSION = "0.1";
+
+use strict;
+use warnings;
+BEGIN {
+    use File::Basename;
+    use lib dirname(__FILE__) . "/lib";
+}
+use HariSekhonUtils;
+use Time::HiRes 'sleep';
+use Term::ReadKey 'GetTerminalSize';
+
+# Original Shell Trick:
+#
+# echo -e "\e[1;40m" ; clear ; while :; do echo $LINES $COLUMNS $(( $RANDOM % $COLUMNS)) $(( $RANDOM % 72 )) ;sleep 0.05; done|gawk '{ letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()"; c=$4; letter=substr(letters,c,1);a[$3]=0;for (x in a) {o=a[x];a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,letter; printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;if (a[x] >= $1) { a[x]=0; } }}'
+#
+# More clearly:
+#
+# echo -e "\e[1;40m";
+# clear; 
+# while :; do
+#     echo $LINES $COLUMNS $(( $RANDOM % $COLUMNS)) $(( $RANDOM % 72 )) ;sleep 0.05; done |
+#     gawk '{
+#         letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()";
+#         c=$4;
+#         letter=substr(letters,c,1);
+#         a[$3]=0;
+#         for (x in a) {
+#             o=a[x];
+#             a[x]=a[x]+1; 
+#             printf "\033[%s;%sH\033[2;32m%s",o,x,letter; 
+#             printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;
+#             if(a[x] >= $1){
+#                 a[x]=0;
+#             }
+#         }
+#     }'
+
+# Perl Reimplementation, much more readable with explanation comments:
+
+get_options();
+my ($columns, $lines, $wpixels, $hpixels) = GetTerminalSize();
+
+$columns = validate_int($columns, 'Terminal columns', 0, 1000);
+$lines   = validate_int($lines,   'Terminal lines',   0, 5000);
+
+my @chars = ("A".."Z", "a".."z", 0..9, split('', '@#$%^&*()'));
+
+my $ESC = "\033";
+
+set_timeout($timeout, sub { printf "${ESC}[%s;%sH${ESC}[0;40m${ESC}[1;37m%s${ESC}[$lines;${columns}H", int($lines / 2.0) , (int($columns / 2.0) - 12), "  ==> SYSTEM ERROR <==  "; exit 0; } );
+
+# sets terminal to bold black
+print "${ESC}[1;40m";
+# clear screen, reset cursor position to 0,0
+print "${ESC}[2J${ESC}[0;0H";
+my ( %a, $letter, $line, $column, $o);
+while(1){
+    $letter = $chars[rand @chars];
+    $a{int(rand $columns)} = 0;
+    foreach $column (keys %a){
+        $o = $a{$column};
+        $a{$column} += 1;
+        # cursor position line $o, $column
+        printf "${ESC}[%s;%sH",   $o, $column;
+        # set bold black bg (1;40), faint;green fg (2;32), print $letter
+        printf "${ESC}[1;40m${ESC}[2;32m%s" , $letter;
+        # cursor position line $a{$column}, column $column
+        printf "${ESC}[%s;%sH",  $a{$column}, $column;
+        # set normal black bg (0;40) to not dim white chars, all them to stand out more
+        # set bold;white fg (1;37), print $letter
+        printf "${ESC}[0;40m${ESC}[1;37m%s", $letter;
+        # reset to 0,0 coordinates
+        #printf "${ESC}[0;0H";
+        if($a{$column} >= $lines){
+            $a{$column} = 0;
+        }
+    }
+    sleep 0.0565;
+}
