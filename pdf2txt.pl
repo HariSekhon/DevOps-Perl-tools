699586f2d67e6face78d0a350720fb0211398766
Hari Sekhon <harisekhon@gmail.com>
2015-03-07 14:06:02 +0000

    committed old pdf2text script from a couple years back

---
+++ pdf2txt.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl -T
+#  vim:ts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2013-02-01 16:22:00 +0000 (Fri, 01 Feb 2013)
+#
+#  License: see accompanying HariSekhon LICENSE
+#
+#  http://github.com/harisekhon/toolbox
+
+$DESCRIPTION="Tool for converting PDF to text for text analytics. Rustled this up quickly in Cloudera to analyze our internal KB for an unofficial topic challenge, which I won as a result of using this and a Java MapReduce word count :)
+
+Operates as a standard unix filter program taking any number of PDF files as arguments (or -f/--pdf comma separated files or PDF content in standard input) and outputting the text to standard output.
+
+See also Apache pdfbox and pdf2text unix tool";
+
+$VERSION = "0.1";
+
+use strict;
+use warnings;
+BEGIN {
+    use File::Basename;
+    use lib dirname(__FILE__) . "/lib";
+}
+use HariSekhonUtils;
+use CAM::PDF;
+use CAM::PDF::PageText;
+
+$github_repo = "toolbox";
+
+my $filename;
+
+%options = (
+    "f|pdf=s" =>  [ \$filename, "PDF File to extract TXT from" ],
+);
+
+get_options();
+my @filenames = split(/,/, $filename) if $filename;
+push(@filenames, @ARGV);
+push(@filenames, "-") unless @filenames;
+
+my $pdf;
+foreach(@filenames){
+    if($_ eq "-"){
+        print "PDF STDIN:\n";
+    } else {
+        print "PDF filename: $_";
+    }
+    $pdf = CAM::PDF->new($_);
+#my $pageone_tree = $pdf->getPageContentTree(4);
+#print CAM::PDF::PageText->render($pageone_tree);
+    unless($pdf){
+        print "No valid PDF detected, skipping...\n";
+        next;
+    }
+    foreach(1..$pdf->numPages()){
+        # TODO: this doesn't really work well, tried getPageContent which works even less well...
+        print "page $_: " . $pdf->getPageText($_) . "\n";
+    }
+}
