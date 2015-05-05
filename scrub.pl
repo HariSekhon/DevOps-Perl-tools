d6436576f558a5ec6e0bd40d41b76d5e06564d0e
Hari Sekhon <harisekhon@gmail.com>
2015-05-05 11:03:50 +0000

    split --host to targeted --hostname/--domain/--fqdn if wanted, exposed --junos, changed scrubbed strings to be more descriptive so you can see what was removed and where the scrub match came from

--- scrub.pl
+++ scrub.pl
@@ -15,7 +15,7 @@ Works like a standard unix filter program, taking input from standard input or f
 
 Create a list of phrases to scrub from config by placing them in scrub_custom.txt in the same directory as this program, one PCRE format regex per line, blank lines and lines prefixed with # are ignored";
 
-$VERSION = "0.5.2";
+$VERSION = "0.6.0";
 
 use strict;
 use warnings;
@@ -27,32 +27,40 @@ use HariSekhonUtils qw /:DEFAULT :regex/;
 
 my $file;
 
-my $all      = 0;
-my $ip       = 0;
+my $all       = 0;
+my $ip        = 0;
 my $ip_prefix = 0;
-my $host     = 0;
-my $network  = 0;
-my $cisco    = 0;
-my $screenos = 0;
-my $custom   = 0;
-my $cr       = 0;
+my $host      = 0;
+my $hostname  = 0;
+my $domain    = 0;
+my $fqdn      = 0;
+my $network   = 0;
+my $cisco     = 0;
+my $screenos  = 0;
+my $junos     = 0;
+my $custom    = 0;
+my $cr        = 0;
 my $skip_java_exceptions = 0;
 
 %options = (
     "f|files=s"     => [ \$file,        "File(s) to scrub, non-option arguments are also counted as files. If no files are given uses standard input stream" ],
-    "a|all"         => [ \$all,         "Apply all scrubbings (careful this includes --host which can be overzealous and match too many things)" ],
+    "a|all"         => [ \$all,         "Apply all scrubbings (careful this includes --host which can be overzealous and match too many things, in which case try more targeted scrubbings below)" ],
     "i|ip"          => [ \$ip,          "Apply IPv4 IP address and Mac address format scrubbing. This and --ip-prefix below can end up matching version numbers, in which case you can switch to putting your network prefix regex in scrub_custom.conf and using just --custom instead" ],
     "ip-prefix"     => [ \$ip_prefix,   "Apply IPv4 IP address prefix scrubbing but leave last octet (for cluster debugging), still applies full Mac address format scrubbing" ],
-    "H|host"        => [ \$host,        "Apply host, domain and fqdn format scrubbing. This will unfortunately scrub Java stack traces of class names also, in which case you should not use --host or --all, instead use --custom and put your domain regex in scrub_custom.conf" ],
-    "n|network"     => [ \$network,     "Apply all network scrubbing, whether Cisco, ScreenOS, JunOS ..." ],
+    "H|host"        => [ \$host,        "Apply host, domain and fqdn format scrubbing (same as -odF). This will unfortunately scrub Java stack traces of class names also, in which case you should not use --domain/--fqdn or --host/--all which include them. Instead use --custom and put your domain regex in scrub_custom.conf" ],
+    "o|hostname"    => [ \$hostname,    "Apply hostname format scrubbing (only works on \"<host>:<port>\" otherwise this would match everything (consider using --custom in addition to remove other references)" ],
+    "d|domain"      => [ \$domain,      "Apply domain format scrubbing" ],
+    "F|fqdn"        => [ \$fqdn,        "Apply fqdn format scrubbing" ],
+    "n|network"     => [ \$network,     "Apply all network scrubbing, whether Cisco, ScreenOS, JunOS for secrets, auth, usernames, passwords, md5s, PSKs, AS, SNMP etc." ],
     "c|cisco"       => [ \$cisco,       "Apply Cisco IOS/IOS-XR/NX-OS configuration format scrubbing" ],
     "s|screenos"    => [ \$screenos,    "Apply Juniper ScreenOS configuration format scrubbing" ],
+    "j|junos"       => [ \$junos,       "Apply Juniper JunOS configuration format scrubbing (limited, please raise a ticket for extra matches to be added)" ],
     "m|custom"      => [ \$custom,      "Apply custom phrase scrubbing (add your Name, Company Name etc to the list of blacklisted words/phrases one per line in scrub_custom.txt). Matching is case insensitive. Recommended to use to work around --host matching too many things" ],
     "r|cr"          => [ \$cr,          "Strip carriage returns ('\\r') from end of lines leaving only newlines ('\\n')" ],
     "e|skip-java-exceptions" => [ \$skip_java_exceptions,  "Skip lines with Java Exceptions from host/fqdn scrubbing to prevent scrubbing java classes needed for debugging stack traces. This is slightly risky as it may potentially miss hostnames/fqdns if colocated on the same lines" ],
 );
 
-@usage_order = qw/files all ip ip-prefix host network cisco screenos custom cr skip-java-exceptions/;
+@usage_order = qw/files all ip ip-prefix host hostname domain fqdn network cisco screenos junos custom cr skip-java-exceptions/;
 get_options();
 if($all){
     $ip       = 1;
@@ -60,8 +68,20 @@ if($all){
     $network  = 1;
     $custom   = 1;
 }
-unless($ip + $ip_prefix + $host + $network + $cisco + $screenos + $custom > 0){
-    usage "must specify a scrubbing to apply";
+unless(
+    $cisco +
+    $custom +
+    $domain +
+    $fqdn +
+    $host +
+    $hostname +
+    $ip +
+    $ip_prefix +
+    $network +
+    $screenos +
+    $junos
+    > 0){
+    usage "must specify one or more scrubbing types to apply";
 }
 ($ip and $ip_prefix) and usage "cannot specify both --ip and --ip-prefix, they are mutually exclusive behaviours";
 
@@ -96,10 +116,14 @@ sub scrub($){
     $string = scrub_ip_prefix ($string) if $ip_prefix;
     $string = scrub_ip      ($string)  if $ip and not $ip_prefix;
     $string = scrub_host    ($string)  if $host;
+    $string = scrub_fqdn    ($string)  if $fqdn     and not $host;
+    $string = scrub_domain  ($string)  if $domain   and not $host;
+    $string = scrub_hostname($string)  if $hostname and not $host;
     $string = scrub_custom  ($string)  if $custom;
     $string = scrub_network ($string)  if $network;
     $string = scrub_cisco   ($string)  if $cisco;
     $string = scrub_screenos($string)  if $screenos;
+    $string = scrub_junos($string)     if $junos;
     return "$string$line_ending";
 }
 
@@ -145,26 +169,53 @@ sub scrub_ip_prefix($){
     return $string;
 }
 
-# TODO: split in to scrub_hostname, scrub_fqdn, scrub_domain
-sub scrub_host($){
+##############################
+# this host based scrubbings will still scrub class names in random debug messages that I can't predict, and there is always risk of not scrubbing sensitive hosts
+#### This is imperfect and a little risky stopping it interfering with Java stack traces this way because it effectively excludes the whole line which may potentially miss legitimiate host regex matches later in the line, will have to watch this
+
+sub skip_java_exceptions($$;$){
     my $string = shift;
-    # this will still scrub class names in random debug messages that I can't predict, and there is always risk of not scrubbing sensitive hosts
-    #### This is imperfect and a little risky stopping it interfering with Java stack traces this way because it effectively excludes the whole line which may potentially miss legitimiate host regex matches later in the line, will have to watch this
+    my $regex  = shift;
+    my $name   = shift || "";
+    $name .= " ";
     if($skip_java_exceptions){
-        return $string if $string =~ /(?:^\s+at|^Caused by:)\s+\w+(?:\.\w+)+/                           and debug "skipping \\sat|^Caused by";
-        return $string if $string =~ /\((?:$hostname_regex|$fqdn_regex|$domain_regex2):[\w-]+\(\d+\)\)/ and debug "skipping (regex):\\w(\\d+)";
-        # overzealous, strips long hostnames that partial match and the beginning and \.\w+ the rest
-        #return $string if $string =~ /^(?:$fqdn_regex)\.\w/                                             and debug "skipping ^regex.\\w+";
-        return $string if $string =~ /\$\w+\((?:$hostname_regex|$fqdn_regex|$domain_regex2):\d+\)/      and debug "skipping \$\\w+(regex)";
+        return 1 if $string =~ /(?:^\s+at|^Caused by:)\s+\w+(?:\.\w+)+/ and debug "skipping $name\\sat|^Caused by";
+        return 1 if $string =~ /\($regex):[\w-]+\(\d+\)\)/              and debug "skipping $name(regex):\\w(\\d+)";
+        return 1 if $string =~ /\$\w+\($regex:\d+\)/                    and debug "skipping $name\$\\w+(regex)";
     }
-    $string =~ s/$fqdn_regex/<fqdn>/go;
-    ####
+    return 0;
+}
+
+sub scrub_hostname($){
+    my $string = shift;
+    return $string if skip_java_exceptions($string, $hostname_regex, "hostname");
     $string =~ s/$hostname_regex:(\d{1,5}(?:[^A-Za-z]|$))/<host>:$1/go;
-    # This currently matches too much stuff
-    # variable length lookbehind is not implemented, so can't use full $tld_regex (which might be too permissive anyway)
+    return $string;
+}
+
+sub scrub_domain($){
+    my $string = shift;
+    return $string if skip_java_exceptions($string, $domain_regex2, "domain");
     $string =~ s/$domain_regex2/<domain>/go;
     return $string;
 }
+
+sub scrub_fqdn($){
+    my $string = shift;
+    return $string if skip_java_exceptions($string, $fqdn_regex, "fqdn");
+    # variable length lookbehind is not implemented, so can't use full $tld_regex (which might be too permissive anyway)
+    $string =~ s/$fqdn_regex/<fqdn>/go;
+    return $string;
+}
+
+sub scrub_host($){
+    my $string = shift;
+    $string = scrub_fqdn($string);
+    $string = scrub_domain($string);
+    $string = scrub_hostname($string);
+    return $string;
+}
+##############################
 
 sub scrub_network($){
     my $string = shift;
@@ -177,19 +228,19 @@ sub scrub_network($){
 
 sub scrub_network_generic($){
     my $string = shift;
-    $string =~ s/username .*/username <scrubbed>/;
-    $string =~ s/syscontact .*/syscontact <scrubbed>/;
+    $string =~ s/username .*/username <username>/;
+    $string =~ s/syscontact .*/syscontact <syscontact>/;
     return $string;
 }
 
 sub scrub_cisco($){
     my $string = shift;
-    $string =~ s/username .+ (?:password|secret) .*?$/username <scrubbed> password <scrubbed>/g;
-    $string =~ s/password .*?$/password <scrubbed>/g;
-    $string =~ s/secret .*?$/secret <scrubbed>/g;
-    $string =~ s/\smd5\s+.*?$/ md5 <scrubbed>/g;
-    $string =~ s/\scommunity\s+.*$/ community <scrubbed>/g;
-    $string =~ s/(standby\s+\d+\s+authentication).*/$1 <scrubbed>/g;
+    $string =~ s/username .+ (?:password|secret) .*?$/username <username> password <password>/g;
+    $string =~ s/password .*?$/password <password>/g;
+    $string =~ s/secret .*?$/secret <secret>/g;
+    $string =~ s/\smd5\s+.*?$/ md5 <md5>/g;
+    $string =~ s/\scommunity\s+.*$/ community <community>/g;
+    $string =~ s/(standby\s+\d+\s+authentication).*/$1 <auth>/g;
     $string =~ s/\sremote-as\s\d+/remote-as <AS>/g;
     $string =~ s/\sdescription\s.*$/description <description>/g;
     $string = scrub_network_generic($string) unless $network;
@@ -200,18 +251,18 @@ sub scrub_screenos($){
     my $string = shift;
     $string =~ s/set admin (name|user|password) "?.+"?/set admin $1 <scrubbed>/g;
     $string =~ s/set snmp (community|host) "?.+"?/set snmp $1 <scrubbed>/g;
-    $string =~ s/ md5 "?.+"?/ md5 <scrubbed>/g;
-    $string =~ s/ key [^\s]+ (?:!enable)/ key <scrubbed>/g;
-    $string =~ s/set nsmgmt init id [^\s]+/set nsmgmt init id <scrubbed>/g;
-    $string =~ s/preshare .+? /preshare <scrubbed> /g;
+    $string =~ s/ md5 "?.+"?/ md5 <md5>/g;
+    $string =~ s/ key [^\s]+ (?:!enable)/ key <key>/g;
+    $string =~ s/set nsmgmt init id [^\s]+/set nsmgmt init id <id>/g;
+    $string =~ s/preshare .+? /preshare <psk> /g;
     $string = scrub_network_generic($string) unless $network;
     return $string;
 }
 
 sub scrub_junos($){
     my $string = shift;
-    $string =~ s/pre-shared-key\s.*/pre-shared-key <scrubbed>/g;
-    $string =~ s/\shome\s+.*/ home <scrubbed>/g;
+    $string =~ s/pre-shared-key\s.*/pre-shared-key <psk>/g;
+    $string =~ s/\shome\s+.*/ home <home>/g;
     $string = scrub_network_generic($string) unless $network;
     return $string;
 }
