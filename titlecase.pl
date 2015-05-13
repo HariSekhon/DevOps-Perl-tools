12d4e49f22dfd0c74482e3939f932d847830023a
Hari Sekhon <harisekhon@gmail.com>
2015-05-13 22:28:57 +0100

    intermediate commit of recase from phrases file

--- titlecase.pl
+++ titlecase.pl
@@ -14,7 +14,7 @@ Does not uppercase letters immediately after a dot except for dotted acronyms (2
 
 Works as a standard unix filter program, taking files are arguments or assuming input from standard input and printing to standard output.";
 
-$VERSION = "0.2";
+$VERSION = "0.3";
 
 use strict;
 use warnings;
@@ -26,16 +26,27 @@ use HariSekhonUtils;
 
 my $file;
 my $lowercase;
+my $recase;
 
 %options = (
     "f|files=s"   => [ \$file,        "File(s) to titlecase, non-option arguments are also counted as files. If no files are given uses standard input stream" ],
-    "l|lowercase" => [ \$lowercase,   "Lowercase the rest of the letters" ],
+    "l|lowercase" => [ \$lowercase,   "Lowercase the rest of the letters (optional)" ],
+    #"r|recase=s"  => [ \$recase,      "ReCase words/phrases from a text file as they appear in that text file one per line (optional)" ],
 );
 @usage_order = qw/files lowercase/;
 
 get_options();
 
 my @files = parse_file_option($file, "args are files");
+
+#my @recase_regexes;
+#if($recase){
+#    my $fh = open_file $recase;
+#    $recase_regex = "(?:";
+#    while my $regex (<$fh>){
+#        push(@recase_regexes, $regex);
+#    }
+#}
 
 sub titlecase ($) {
     my $string = shift;
@@ -44,12 +55,17 @@ sub titlecase ($) {
     $string =~ s/\b(?<![\.'])([A-Za-z])/\U$1/g;
     # uppercase acronyms to correct for the above exception for file extensions
     $string =~ s/(\s[A-Za-z](?:\.[A-Za-z])+)/\U$1/g;
+#    if(@recase_regexes){
+#        foreach my $regex (@recase_regexes){
+#            s/$regex/$regex/ig and last;
+#        }
+#    }
     print $string;
 }
 
 if(@files){
     foreach my $file (@files){
-        open(my $fh, $file) or die "Failed to open file '$file': $!\n";
+        my $fh = open_file $file;
         while(<$fh>){ titlecase($_) }
     }
 } else {
