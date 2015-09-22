7c563d662fadc338a005ec496bc26ddf64721812
Hari Sekhon <harisekhon@gmail.com>
2015-09-22 17:09:27 +0100

    added tests/sqlcase.sh

---
+++ tests/sqlcase.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-07-28 18:47:41 +0100 (Tue, 28 Jul 2015)
+#
+#  https://github.com/harisekhon
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+
+set -eu
+
+if echo "select * from blah" | `dirname $0`/../sqlcase.pl | tee /dev/stderr | grep -q 'SELECT \* FROM blah'; then
+    echo "recasing of SQL succeeded"
+ else
+    echo "recasing of SQL FAILED"
+    exit 1
+fi
