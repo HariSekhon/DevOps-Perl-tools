adf4278052f14b82cf70d573b6a93670b2c88979
Hari Sekhon <harisekhon@gmail.com>
2015-11-02 21:27:52 +0000

    added dockercase.sh

---
+++ tests/dockercase.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-07-28 18:47:41 +0100 (Tue, 28 Jul 2015)
+#
+#  https://github.com/harisekhon
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+
+set -eu
+
+checksum='75055c31342ba35ea59c4091fff098c7'
+
+`dirname $0`/../dockercase.pl Dockerfile > Dockerfile2
+if [ "`uname -s`" = 'Darwin' -a "`md5 -q Dockerfile2`"    = "$checksum" ] ||
+   [ "`uname -s`" = 'Linux'  -a "`md5sum -q Dockerfile2`" = "$checksum" ]; then
+    echo "recasing of Dockerfile succeeded"
+ else
+    echo "recasing of Dockerfile FAILED"
+    exit 1
+fi
