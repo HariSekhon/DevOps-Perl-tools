87b5eee1323bb7b1bb07611592145a63d9e8f426
Hari Sekhon <harisekhon@gmail.com>
2015-11-02 21:29:08 +0000

    updated dockercase.sh

--- tests/dockercase.sh
+++ tests/dockercase.sh
@@ -13,7 +13,9 @@ set -eu
 
 checksum='75055c31342ba35ea59c4091fff098c7'
 
-`dirname $0`/../dockercase.pl Dockerfile > Dockerfile2
+pushd `dirname $0` >/dev/null || exit 1
+
+../dockercase.pl Dockerfile > Dockerfile2
 if [ "`uname -s`" = 'Darwin' -a "`md5 -q Dockerfile2`"    = "$checksum" ] ||
    [ "`uname -s`" = 'Linux'  -a "`md5sum -q Dockerfile2`" = "$checksum" ]; then
     echo "recasing of Dockerfile succeeded"
@@ -21,3 +23,5 @@ if [ "`uname -s`" = 'Darwin' -a "`md5 -q Dockerfile2`"    = "$checksum" ] ||
     echo "recasing of Dockerfile FAILED"
     exit 1
 fi
+rm -f Dockerfile2
+popd >/dev/null
