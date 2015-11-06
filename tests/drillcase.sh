88b561101f24b438c9a26a9ab0bc9f150032a2c3
Hari Sekhon <harisekhon@gmail.com>
2015-11-06 00:46:09 +0000

    added tests/drillcase.sh

---
+++ tests/drillcase.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-07-29 15:05:43 +0100 (Wed, 29 Jul 2015)
+#
+#  https://github.com/harisekhon
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -eu
+srcdir=`dirname $0`
+
+cd $srcdir/..
+
+if echo "select columns[0] from myTable where name = 'hari';" | ./drillcase.pl | tee /dev/stderr | grep -qF "SELECT columns[0] FROM myTable WHERE name = 'hari';"; then
+    echo "recasing of Drill statement succeeded"
+else
+    echo "recasing of Drill statement FAILED"
+    exit 1
+fi
