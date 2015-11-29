c52ac66be4d375e7cb4ea23140e8401beec5db56
Hari Sekhon <harisekhon@gmail.com>
2015-11-29 23:23:39 +0000

    updated tests/test_scrub.sh

--- tests/test_scrub.sh
+++ tests/test_scrub.sh
@@ -37,14 +37,33 @@ dest[5]='21 Sep 2015 02:28:45,580  INFO [qtp-ambari-agent-6292] HeartBeatHandler
 src[6]='21 Sep 2015 14:54:44,811  WARN [ambari-action-scheduler] ActionScheduler:311 - Operation completely failed, aborting request id:113'
 dest[6]='21 Sep 2015 14:54:44,811  WARN [ambari-action-scheduler] ActionScheduler:311 - Operation completely failed, aborting request id:113'
 
-for (( i = 0 ; i < ${#src[@]} ; i++ )); do
-    result="$($perl -T $I_lib ./scrub.pl -ae <<< "$src[$i]")"
-    if grep -Fq "$dest[$i]" <<< "$result"; then
+src[7]="curl  -iuadmin:'mysecret' 'http://myServer:8080/...'"
+dest[7]="curl  -iu<user>:<password> 'http://<hostname>:8080/...'"
+
+src[8]="curl  -u admin:mysecret 'http://myServer:8080/...'"
+dest[8]="curl  -u <user>:<password> 'http://<hostname>:8080/...'"
+
+src[9]="curl  -u admin:'my secret' 'http://myServer:8080/...'"
+dest[9]="curl  -u <user>:<password> 'http://<hostname>:8080/...'"
+
+src[10]="curl  -u admin:\"my secret\" 'http://myServer:8080/...'"
+dest[10]="curl  -u <user>:<password> 'http://<hostname>:8080/...'"
+
+src[11]="curl -u=admin:'mysecret' 'http://myServer:8080/...'"
+dest[11]="curl -u=<user>:<password> 'http://<hostname>:8080/...'"
+
+# suport sparse arrays so that we can easily comment out any check pair for convenience
+# this gives the number of elements and prevents testing the last element(s) if commenting something out in the middle
+#for (( i = 0 ; i < ${#src[@]} ; i++ )); do
+for i in ${!src[@]}; do
+    #[ -z "${src[$i]:-}" ] && { echo "skipping test $i..."; continue; }
+    result="$($perl -T $I_lib ./scrub.pl -ae <<< "${src[$i]}")"
+    if grep -Fq "${dest[$i]}" <<< "$result"; then
         echo "SUCCEEDED scrubbing test $i"
     else
-        echo "FAILED to scrub line"
-        echo "input:    $src[$i]"
-        echo "expected: $dest[$i]"
+        echo "FAILED to scrub line during test $i"
+        echo "input:    ${src[$i]}"
+        echo "expected: ${dest[$i]}"
         echo "got:      $result"
         exit 1
     fi
