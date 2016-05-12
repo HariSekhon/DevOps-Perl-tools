afd0bc8614da7c1237af1528aba294f0700987a6
Hari Sekhon <harisekhon@gmail.com>
2016-05-12 23:51:41 +0100

    updated welcome.pl

--- welcome.pl
+++ welcome.pl
@@ -3,9 +3,14 @@
 #  Author: Hari Sekhon
 #  Date: 2009-12-10 17:57:49 +0000 (Thu, 10 Dec 2009)
 #
-#  http://github.com/harisekhon/tools
+#  https://github.com/harisekhon/tools
 #
-#  License: see accompanying LICENSE file
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
 #
 
 $DESCRIPTION = "Prints a slick welcome message with last login time
