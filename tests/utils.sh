3b067845d79f4c7dd0ced172223192111636a065
Hari Sekhon <harisekhon@gmail.com>
2016-06-16 15:04:32 +0100

    updated tests/utils.sh

--- tests/utils.sh
+++ tests/utils.sh
@@ -18,6 +18,10 @@ set -eu
 [ -n "${DEBUG:-}" ] && set -x
 srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+. "$srcdir/excluded.sh"
+. "$srcdir/../bash-tools/utils.sh"
+. "$srcdir/../bash-tools/docker.sh"
+
 hr(){
     echo "===================="
 }
