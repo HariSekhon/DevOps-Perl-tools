23f2bd17c7b91c6844fece9b86ebe87a304bfb4c
Hari Sekhon <harisekhon@gmail.com>
2016-08-13 22:11:03 +0100

    updated utils.sh

--- tests/utils.sh
+++ tests/utils.sh
@@ -27,37 +27,35 @@ hr(){
 }
 
 # Taint code doesn't use PERL5LIB, use -I instead
-I_lib=""
+#I_lib=""
 
-perl=""
+perl="perl"
 
 if [ -n "${PERLBREW_PERL:-}" ]; then
     PERL_VERSION="${PERLBREW_PERL}"
     PERL_VERSION="${PERLBREW_PERL/perl-/}"
 
     # For Travis CI which installs modules locally
-    export PERL5LIB=$(echo \
-        ${PERL5LIB:-.} \
-        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/site_perl/$PERL_VERSION/x86_64-linux \
-        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/site_perl/$PERL_VERSION/darwin-2level \
-        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/site_perl/$PERL_VERSION \
-        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/$PERL_VERSION/x86_64-linux \
-        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/$PERL_VERSION/darwin-2level \
-        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/$PERL_VERSION \
-        | tr '\n' ':'
-    )
-
-    for x in $(echo "$PERL5LIB" | tr ':' ' '); do
-        I_lib+="-I $x "
-    done
+#    export PERL5LIB=$(echo \
+#        ${PERL5LIB:-.} \
+#        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/site_perl/$PERL_VERSION/x86_64-linux \
+#        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/site_perl/$PERL_VERSION/darwin-2level \
+#        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/site_perl/$PERL_VERSION \
+#        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/$PERL_VERSION/x86_64-linux \
+#        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/$PERL_VERSION/darwin-2level \
+#        $PERLBREW_ROOT/perls/$PERLBREW_PERL/lib/$PERL_VERSION \
+#        | tr '\n' ':'
+#    )
+#
+#    for x in $(echo "$PERL5LIB" | tr ':' ' '); do
+#        I_lib+="-I $x "
+#    done
 
     sudo=sudo
     #perl="/home/travis/perl5/perlbrew/perls/$TRAVIS_PERL_VERSION/bin/perl"
-    perl=perl
     PERL_MAJOR_VERSION="$($perl -v | $perl -ne '/This is perl (\d+), version (\d+),/ && print "$1.$2"')"
 else
     sudo=""
-    perl=perl
     PERL_MAJOR_VERSION="$($perl -v | $perl -ne '/This is perl (\d+), version (\d+),/ && print "$1.$2"')"
 fi
 
