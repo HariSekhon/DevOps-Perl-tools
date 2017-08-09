917e21da6cc78a140e084043597d3a297188a044
Hari Sekhon <harisekhon@gmail.com>
2017-08-09 16:26:48 +0100

    updated test_docker.sh

--- tests/test_docker.sh
+++ tests/test_docker.sh
@@ -20,12 +20,13 @@ srcdir="$(cd "$(dirname "$0")" && pwd)"
 cd "$srcdir/.."
 
 . "bash-tools/docker.sh"
+. "bash-tools/utils.sh"
 
 section "Docker Image"
 
 export DOCKER_IMAGE="harisekhon/tools"
 
-if is_docker_available; then
+if is_CI && is_docker_available; then
     docker pull "$DOCKER_IMAGE"
     docker run "$DOCKER_IMAGE" tests/all.sh
 fi
