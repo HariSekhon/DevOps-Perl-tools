9d6aea2f5c037794ee68b4210f48aa81f8e890cb
Hari Sekhon <harisekhon@gmail.com>
2017-09-08 12:31:57 +0200

    removed whitespace only lines to pass test suites

--- contrib/hdfs-du-viz.sh
+++ contrib/hdfs-du-viz.sh
@@ -6,14 +6,14 @@
 #
 
 max_depth=5
- 
+
 largest_root_dirs=$(hdfs dfs -du -s '/*' | sort -nr | perl -ane 'print "$F[1] "')
- 
+
 printf "%15s  %s\n" "bytes" "directory"
 for ld in $largest_root_dirs; do
     printf "%15.0f  %s\n" $(hdfs dfs -du -s $ld| cut -d' ' -f1) $ld
     all_dirs=$(hdfs dfs -ls -R $ld | egrep '^dr........' | perl -ane "scalar(split('/',\$_)) <= $max_depth && print \"\$F[7]\n\"" )
- 
+
     for d in $all_dirs; do
         line=$(hdfs dfs -du -s $d)
         size=$(echo $line | cut -d' ' -f1)
