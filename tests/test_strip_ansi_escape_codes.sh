0b63a8aceba5dbd99a040e2adbdac77ab6a0b802
Hari Sekhon <harisekhon@gmail.com>
2018-09-09 23:03:58 +0100

    added tests/test_strip_ansi_escape_codes.sh

---
+++ tests/test_strip_ansi_escape_codes.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-07-28 18:47:41 +0100 (Tue, 28 Jul 2015)
+#
+#  https://github.com/harisekhon/tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+
+set -eu
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$srcdir/..";
+
+. ./tests/utils.sh
+
+section "Strip ANSI Escape Codes"
+
+start_time=$(date +%s)
+
+if is_mac; then
+    cat_opts="-e"
+else
+    cat_opts="-A"
+fi
+if echo "some highlighted content" |
+    grep --color=yes highlighted |
+    $perl -T ./strip_ansi_escape_codes.pl |
+    tee /dev/stderr |
+    cat $cat_opts |
+    tee /dev/stderr |
+    grep -q '^some highlighted content$'; then
+    echo "ANSI escape code stripping SUCCEEDED"
+ else
+    echo "ANSI escape code stripping SUCCEEDED"
+    exit 1
+fi
+
+echo
+echo "Total Tests run: $total_run_count"
+time_taken "$start_time" "All version tests for $name completed in"
+echo
