bcf880e157f43b5f2ceb539210e2a36b51b5413a
Hari Sekhon <harisekhon@gmail.com>
2019-09-27 13:25:45 +0100

    added perlpath.pl

---
+++ perlpath.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+#
+#  Author: Hari Sekhon
+#  Date: 2019-09-27
+#
+#  https://github.com/harisekhon/devops-perl-tools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn
+#  and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+$DESCRIPTION = "
+
+Simple tool to print the Perl \@INC path, one per line
+
+Tested on Perl 5.x on Mac and Linux
+
+";
+
+$VERSION = "0.1.0";
+
+use strict;
+use warnings;
+#BEGIN {
+#    use File::Basename;
+#    use lib dirname(__FILE__) . "/lib";
+#}
+
+print join("\n", @INC);
