4de671eff17872157fe3a4e7580520b8eb7df987
Hari Sekhon <harisekhon@gmail.com>
2019-10-02 23:16:38 +0100

    updated perlpath.pl

--- perlpath.pl
+++ perlpath.pl
@@ -25,9 +25,26 @@ $VERSION = "0.1.0";
 
 use strict;
 use warnings;
+use File::Basename;
 #BEGIN {
 #    use File::Basename;
 #    use lib dirname(__FILE__) . "/lib";
 #}
+
+sub usage(){
+    my $progname = basename $0;
+    my $description = $main::DESCRIPTION;
+    $description =~ s/^\s*//;
+    $description =~ s/\s*$//;
+    print "$description\n\n\n";
+    print "usage: $progname\n\n";
+    exit 3;
+
+}
+
+
+if(@ARGV){
+    usage;
+}
 
 print join("\n", @INC);
