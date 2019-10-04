0547043cde11139dadae5d0543493792a0b19350
Hari Sekhon <harisekhon@gmail.com>
2019-10-04 11:20:17 +0100

    updated tests/excluded.sh

--- tests/excluded.sh
+++ tests/excluded.sh
@@ -35,7 +35,7 @@ isExcluded(){
     fi
     # this external git check is expensive, skip it when in CI as using fresh git checkouts
     is_CI && return 1
-    if which git &>/dev/null; then
+    if type -P git &>/dev/null; then
         commit="$(git log "$prog" | head -n1 | grep 'commit')"
         if [ -z "$commit" ]; then
             return 0
