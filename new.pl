4131d6fa6d076eb39b6a85d868818cbe2efe6303
Hari Sekhon <harisekhon@gmail.com>
2022-08-15 09:51:24 +0100

    updated new.pl

--- new.pl
+++ new.pl
@@ -76,7 +76,7 @@ winfile     Windows file
 If type is omitted, it is taken from the file extension, otherwise it defaults to unix file
 ";
 
-$VERSION = "0.9.2";
+$VERSION = "0.9.3";
 
 use strict;
 use warnings;
@@ -236,8 +236,12 @@ sub get_template($$){
     my $dirname = abs_path dirname $filename;
 
     # Special Rules - to be mostly avoided in favour of uniform conventions
-    if($dirname =~ /\/\.github\/workflows$/ and $ext =~ /^ya?ml$/){
-        $base_filename = "github-workflow.yaml";
+    if($ext =~ /^ya?ml$/){
+        if($dirname =~ /\/\.github\/workflows$/ and $ext =~ /^ya?ml$/){
+            $base_filename = "github-workflow.yaml";
+        } elsif ($filename =~ /patch/){
+            $base_filename = "patch.yaml"
+        }
     } elsif($plugin and $ext eq "pl"){
         $base_filename = "template-plugin.pl";
     }
